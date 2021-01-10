e944315a79483a5cc7eb8760d073dcab4c3cc6e5
valenottaviano <valentinottaviano@gmail.com>
2021-01-10 14:23:17 -0300

    more companies added

--- components/PortfolioSection.jsx
+++ components/PortfolioSection.jsx
@@ -6,8 +6,11 @@ export default function PortfolioSection() {
             <div className="portfolio-container">
               <h2>Algunos de nuestros clientes</h2>
               <div className="portfolio-images">
-                <a href="#" ><img src="./img/portfolio-img/spektrum.svg" alt="spektrum" /></a>
-                <a href="https://www.nailtucuman.com.ar " ><img src="./img/portfolio-img/nail.svg" alt="nail" /></a>
+                <a href="https://spektrum.es" ><img src="./img/portfolio-img/spektrum.svg" alt="spektrum" /></a>
+                <a href="#" ><img src="./img/portfolio-img/marce.svg" alt="Marcela Barros IFBB PRO" /></a>
+
+                <a href="https://quelopaleo.com.ar" ><img src="./img/portfolio-img/quelopaleo.svg" alt="Que lo paleó!" /></a>
+                <a href="https://www.nailtucuman.com.ar " ><img src="./img/portfolio-img/nail.svg" alt="Nail Tucumán" /></a>
                 <a href="https://www.cefgroup.com.ar" ><img src="./img/portfolio-img/cef.svg" alt="cefgroup" /></a>
               </div>
             </div>
