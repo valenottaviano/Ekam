b326f9f0c06c0eae9e8c6fce5d26dbffa56576e3
valenottaviano <valentinottaviano@gmail.com>
2021-01-10 14:44:29 -0300

    fixing

--- components/PortfolioSection.jsx
+++ components/PortfolioSection.jsx
@@ -8,7 +8,6 @@ export default function PortfolioSection() {
               <div className="portfolio-images">
                 <a href="https://spektrum.es" ><img src="./img/portfolio-img/spektrum.svg" alt="spektrum" /></a>
                 <a href="#" ><img src="./img/portfolio-img/marce.svg" alt="Marcela Barros IFBB PRO" /></a>
-
                 <a href="https://quelopaleo.com.ar" ><img src="./img/portfolio-img/quelopaleo.svg" alt="Que lo paleó!" /></a>
                 <a href="https://www.nailtucuman.com.ar " ><img src="./img/portfolio-img/nail.svg" alt="Nail Tucumán" /></a>
                 <a href="https://www.cefgroup.com.ar" ><img src="./img/portfolio-img/cef.svg" alt="cefgroup" /></a>
